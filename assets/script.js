2bc517b3396b1fea33a86592cff612d05ce45248
Martha A <mladame43@gmail.com>
2022-08-20 13:33:13 -0700

    password length prompt displaying

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,26 @@
 // Assignment code here
 
-//presented with prompts for password criteria
-//need: click button event to prompt confirm windows
-//need: confirm windows for: uppercase, lowercase, numbers, special characters
-//need: length entry prompt
-//need: input validated with at least one character type selected
-//need: password generated once all prompts are answered
-//need: password displayed, locals storage
+// presented with prompts for password criteria
+// need: click button event to prompt confirm windows
+// need: confirm windows for: uppercase, lowercase, numbers, special characters
+//    prompt: would you like to include uppercase characters > true/false
+// need to define variables possible
+// need: length entry prompt
+//    enter variable between 8 - 128
+// need: input validated with at least one character type selected
+//    if all false, alert(pick at least one variable) (tick possible?)
+//    password must be random from selected variables
+// need: password generated once all prompts are answered
+// need: password displayed, local storage
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// define variables (consolidate? array?)
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numerals = "1234567890";
+var specCharacters = "!#$%&'()*+,-./:;<=>?@][^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
@@ -17,8 +28,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+    var passwordLength = window.prompt("How many characters will your password be? Type a number 8 - 128...");
+
+  if (!userChoice) {
+    return;
+  }
+}
+) 
